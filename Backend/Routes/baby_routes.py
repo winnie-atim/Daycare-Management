9dc86241ee60290d008deb9d280af5c182a99e81
winnie-atim <atwinnie21@gmail.com>
2024-05-18 14:14:58 +0300

    getting baby by access

--- Backend/Routes/baby_routes.py
+++ Backend/Routes/baby_routes.py
@@ -8,7 +8,8 @@ from Controllers.baby_controller import (
     get_all_babies,
     add_baby_to_present,
     get_present_babies,
-    get_released_babies
+    get_released_babies,
+    get_baby_by_access
 )
 
 router = APIRouter()
@@ -65,6 +66,17 @@ async def get_present_babies_route(db: Session = Depends(get_db)):
         present_babies = get_present_babies(db)
         if present_babies:
             return present_babies
+        else:
+            raise HTTPException(status_code=400, detail="An error occurred")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+
+@router.get("/get_baby_by_access")
+async def get_baby_by_access_route(baby_access: str, db: Session = Depends(get_db)):
+    try:
+        baby = get_baby_by_access(db, baby_access)
+        if baby:
+            return baby
         else:
             raise HTTPException(status_code=400, detail="An error occurred")
     except Exception as e:
